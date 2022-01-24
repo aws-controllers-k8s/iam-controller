713b375f55100bbf6b5045f87de0483bbda72132
Vijay Tripathi <vijay.trip40@gmail.com>
2022-01-24 12:39:47 -0800

    ACk runtime upgrade v0.15.2 => v0.16.2 (#13)

--- pkg/resource/role/delta.go
+++ pkg/resource/role/delta.go
@@ -83,7 +83,7 @@ func newResourceDelta(
 			delta.Add("Spec.PermissionsBoundary", a.ko.Spec.PermissionsBoundary, b.ko.Spec.PermissionsBoundary)
 		}
 	}
-	if !reflect.DeepEqual(a.ko.Spec.Policies, b.ko.Spec.Policies) {
+	if !ackcompare.SliceStringPEqual(a.ko.Spec.Policies, b.ko.Spec.Policies) {
 		delta.Add("Spec.Policies", a.ko.Spec.Policies, b.ko.Spec.Policies)
 	}
 	if !reflect.DeepEqual(a.ko.Spec.Tags, b.ko.Spec.Tags) {
