7c9df90b2fdab2b70d495d2fa79ea58c0abe6d62
Amine <hilalyamine@gmail.com>
2022-11-07 14:47:49 -0800

    Add hooks to set `User` and `Group` paths in their update requests (#46)
    The code-generator isn't able to generate correct code for
    `newUpdateRequestPayload`. More specifically it doesn't set the
    `spec.path` to `input.NewPath`. The reason is that the code-generator
    expects the SDK field and CR field names to be similar.
    
    Currently we have a feature that allows the code-generator to set a
    field from a different SDK field name. But not the other way around.
    
    We can discuss the above in https://github.com/aws-controllers-k8s/community/issues/1532
    
    By submitting this pull request, I confirm that my contribution is made under the terms of the Apache 2.0 license.

--- pkg/resource/user/sdk.go
+++ pkg/resource/user/sdk.go
@@ -321,6 +321,17 @@ func (rm *resourceManager) sdkUpdate(
 	if err != nil {
 		return nil, err
 	}
+	// The code-generator expect the SDK field and the CR field to have
+	// exact matching names. Which is not the case for `Path` (CR field)
+	// and `NewPath` (SDK field). It is currently possible to set a CR
+	// field from a custom field but the other way around. For now we
+	// set those manually and wait for a better solution.
+	//
+	// TODO(A-Hilaly): remove once aws-controllers-k8s/community#1532
+	// or better idea is implemented
+	if desired.ko.Spec.Path != nil {
+		input.NewPath = desired.ko.Spec.Path
+	}
 
 	var resp *svcsdk.UpdateUserOutput
 	_ = resp
