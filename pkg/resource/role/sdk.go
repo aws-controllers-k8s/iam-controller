e2066b383e7b75a65cab55cefc629362f5cf2232
Jack Dockerty <jdockerty@influxdata.com>
2023-04-03 12:33:01 +0100

    chore: run code generator

--- pkg/resource/role/sdk.go
+++ pkg/resource/role/sdk.go
@@ -412,8 +412,13 @@ func (rm *resourceManager) sdkUpdate(
 			return nil, err
 		}
 	}
-
-	if !delta.DifferentExcept("Spec.Tags", "Spec.Policies", "Spec.InlinePolicies", "Spec.PermissionsBoundary") {
+	if delta.DifferentAt("Spec.AssumeRolePolicyDocument") {
+		err = rm.syncAssumeRolePolicies(ctx, desired)
+		if err != nil {
+			return nil, err
+		}
+	}
+	if !delta.DifferentExcept("Spec.Tags", "Spec.Policies", "Spec.InlinePolicies", "Spec.PermissionsBoundary", "Spec.AssumeRolePolicyDocument") {
 		return desired, nil
 	}
 
