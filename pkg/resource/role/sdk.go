1107caae0261c0d871553aafee991d05d003b0c8
michaelhtm <98621731+michaelhtm@users.noreply.github.com>
2025-05-08 13:02:49 -0700

    fix: add policies on create

--- pkg/resource/role/sdk.go
+++ pkg/resource/role/sdk.go
@@ -324,6 +324,18 @@ func (rm *resourceManager) sdkCreate(
 			ko.Spec.AssumeRolePolicyDocument = &doc
 		}
 	}
+	for _, p := range desired.ko.Spec.Policies {
+		err := rm.addManagedPolicy(ctx, &resource{ko}, p)
+		if err != nil {
+			return &resource{ko}, err
+		}
+	}
+	for n, p := range desired.ko.Spec.InlinePolicies {
+		err := rm.addInlinePolicy(ctx, &resource{ko}, n, p)
+		if err != nil {
+			return &resource{ko}, err
+		}
+	}
 	ackcondition.SetSynced(&resource{ko}, corev1.ConditionFalse, nil, nil)
 
 	return &resource{ko}, nil
--- templates/hooks/role/sdk_create_post_set_output.go.tpl
+++ templates/hooks/role/sdk_create_post_set_output.go.tpl
@@ -5,4 +5,16 @@
             ko.Spec.AssumeRolePolicyDocument = &doc
         }
     }
+	for _, p := range desired.ko.Spec.Policies {
+		err := rm.addManagedPolicy(ctx, &resource{ko}, p)
+		if err != nil {
+			return &resource{ko}, err
+		}
+	}
+	for n, p := range desired.ko.Spec.InlinePolicies {
+		err := rm.addInlinePolicy(ctx, &resource{ko}, n, p)
+		if err != nil {
+			return &resource{ko}, err
+		}
+	}
     ackcondition.SetSynced(&resource{ko}, corev1.ConditionFalse, nil, nil)
