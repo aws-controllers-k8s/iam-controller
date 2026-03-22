89a51dd4ec03671c43e938448f05956c515b2678
Gustavo Diaz <gustidia@amazon.com>
2026-03-22 01:53:50 +0000

    Create policies manager

--- cmd/controller/main.go
+++ cmd/controller/main.go
@@ -42,6 +42,7 @@ import (
 	_ "github.com/aws-controllers-k8s/iam-controller/pkg/resource/group"
 	_ "github.com/aws-controllers-k8s/iam-controller/pkg/resource/instance_profile"
 	_ "github.com/aws-controllers-k8s/iam-controller/pkg/resource/open_id_connect_provider"
+	_ "github.com/aws-controllers-k8s/iam-controller/pkg/resource/policies"
 	_ "github.com/aws-controllers-k8s/iam-controller/pkg/resource/policy"
 	_ "github.com/aws-controllers-k8s/iam-controller/pkg/resource/role"
 	_ "github.com/aws-controllers-k8s/iam-controller/pkg/resource/service_linked_role"
